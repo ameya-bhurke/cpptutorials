ae36cd5b2ac8d79b067f998d4c922774211ce2cd
Ameya Bhurke <ameyabhurke@gmail.com>
2018-05-10 21:38:41 +1000

    makefile and sample test program for normal distribution

---
+++ normal_distribution.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+// Program that calculates all the metrics for a normal distribution 
+
+// mean                 - arithmetic mean or average of the sample set
+// standard deviation   - square root of the average of squared differences from the mean 
+
+int main()
+{
+    exit(0);
+}
